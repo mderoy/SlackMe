a424925b9b97b01e173a4596336224bdf85ed383
Mike Voorhees <michaelv@unity3d.com>
2017-06-09 09:37:10 -0400

    Make it great
    * Prevent windows crash pop up.  Prevents slackme from never completing
    when a program crashes
    
    * Use Unity's Shell for process running. Removes the need for powershell
    
    * Fix relative command path not working
    
    * Raise last lines to 50

---
+++ slackme/Utils/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace slackme.Utils
+{
+	internal static class Helpers
+	{
+		internal static string AggregateWithSpace(this IEnumerable<string> elements)
+		{
+			return elements.AggregateWith(" ");
+		}
+
+		internal static string AggregateWith(this IEnumerable<string> elements, string separator)
+		{
+			if (elements.Any())
+				return elements.Aggregate((buff, s) => buff + separator + s);
+
+			return string.Empty;
+		}
+	}
+}
